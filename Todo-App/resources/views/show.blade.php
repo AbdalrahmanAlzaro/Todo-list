1617d38043620c5c2ebfa6d765a2f8d4a42681c8
AbdalrahmanAlzaro <abdalrahman.h.alzaro@gmail.com>
2024-05-30 14:45:43 +0300

    create a connection with the DB

---
+++ Todo-App/resources/views/show.blade.php
@@ -0,0 +1,22 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    <h1>
+        {{$task->title}}
+    </h1>  
+    <p>
+        {{$task->description}}
+    </p>
+    @if($task->long_description)
+        <p>{{$task->long_description}}</p>
+    @endif
+
+    <p>{{$task->created_at}}</p>
+    <p>{{$task->updated_at}}</p>
+ </body>
+ </html>
